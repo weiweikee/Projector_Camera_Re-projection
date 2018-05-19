eeddd27600bac6655688608d1fdbd0b32e3d793b
Chi Wei Wei <weiweikee@gmail.com>
2018-05-19 15:11:23 -0400

    Update Projector_Camera_Re-projection.py

--- Projector_Camera_Re-projection/Projector_Camera_Re-projection.py
+++ Projector_Camera_Re-projection/Projector_Camera_Re-projection.py
@@ -6,25 +6,27 @@ from goprocam import constants
 
 #### GoPro Settings ####
 gpCam = GoProCamera.GoPro()
-# gpCam.overview()
-# gpCam.gpControlSet(constants.Stream.BIT_RATE, constants.Stream.BitRate.B2_4Mbps)
-#gpCam.gpControlSet(constants.Stream.WINDOW_SIZE, constants.Stream.WindowSize.W480)
-# cap = cv2.VideoCapture("udp://127.0.0.1:10000")
+gpCam.overview()
 ########################
 
+## Create Projector Window 
 cv2.namedWindow("Projector", cv2.WND_PROP_FULLSCREEN)
 cv2.moveWindow("Projector", 3500, 0)
 cv2.setWindowProperty("Projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+initialPhoto = False
 camCalib = False
 camCalibPic = False
+findCorners = False
 loop = 0
+
+## Crop Variables
 realW = None
 realH = None
 realX = None
 realY = None
-findCorners = False
 
+## Get Calibration Data
 def load_calibration_data():
     print ("reading calibration file")
     calibrationDataFile = np.load("calibration.npz")
@@ -36,17 +38,27 @@ def load_calibration_data():
 while True:
     proj_img = cv2.imread("CircleGrid_3416x1920.jpg", 0)
 
+    ## Take photo without Projection
+    if initialPhoto == False:
+        unProjectedImg = gpCam.take_photo(1)
+        initialPhoto = True
+
+    ## Show Projector Image if the corners are not found
     if findCorners == False:
         cv2.imshow('Projector', proj_img) 
-        
-    if camCalib == False and loop > 1:
+    
+
+    if camCalib == False and loop > 1:\
+
         dist, K = load_calibration_data()
-        print(dist)
-        print(K)
+        
+        ## Take the picture of dotted grid
         if camCalibPic == False:
             print("Taking Picture")
             imgURL = gpCam.take_photo(1)
             camCalibPic = True
+
+        ## Perform Bounding Box and Match the dotted grid with real image
         else:
             if findCorners == False:
                 url = urllib.request.urlopen(imgURL)
@@ -60,23 +72,27 @@ while True:
                 x, y, w, h = roi
                 img = dst[y:y+h, x:x+w]
                 h, w = img.shape[:2]
+                ## Crop the distorted part out 
                 img = img[300:h-300, 300:w-300]
-
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
                 ###### Find estimated Shape ######
-                ret, thresh = cv2.threshold(gray, 135, 255,0) #135, 255 for brighter room #155, 255 for darker room
+                ret, thresh = cv2.threshold(gray, 135, 255,0) ## (gray, 135, 255, 0) for brighter room  ## (gray, 155, 255, 0) for darker room
+                ## find all contours
                 newImg, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                print(len(contours))
+
                 for i in range(len(contours)):
                     cnt = contours[i]
+                    # Find bounding box for all contours
                     x,y,w,h = cv2.boundingRect(cnt)
-                    # find only size rectangle bigger than 200 by 200
+                    # Get bounding box bigger than 200 by 200 and set the crop variables
                     if w > 200 and h > 200:
                         realW = w
                         realX = x
                         realY = y
                         realH = h
 
+                # Draw Rectangle on the bounding box
                 if realW != None and realH != None:
                     cv2.rectangle(gray, (realX,realY), (realX+realW, realY+realH), (0,255,0), 2)
 
@@ -86,11 +102,13 @@ while True:
                 # see the threshold image
                 cv2.imshow('Gray', thresh)
 
+    # Loop is required because projection is slower than the picture taking so 1 loop is a buffer
     loop += 1
 
+    ## if corners
     if findCorners:       
         proj_img = img
-        # reproject the projector image from capture image
+        # reproject the projector image from capture image 
         proj_img = proj_img[realY:realY+realH, realX:realX+realW]
         proj_w, proj_h = proj_img.shape[:2]
         cv2.imshow('Projector', proj_img) 
